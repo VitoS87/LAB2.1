cfd72c29bc57845186eb851d3b5593d12decb61d
Vitos <rustamminis@gmail.com>
2025-10-12 15:40:09 +0200

    Add files via upload

---
+++ LAB2.1/LAB2.1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите сумму грн: ");
+long x = long.Parse(Console.ReadLine());
+long y;
+if (x>400 && x<600) { y = x - (x / 100 * 5); Console.WriteLine("Стоимость покупки: "+y+" грн"); }
+if (x>600 && x<1000) { y = x - (x / 100 * 10); Console.WriteLine("Стоимость покупки: "+y+" грн"); }
+if (x<400 || x>1000) { Console.WriteLine("Стоимость покупки: "+x+" грн"); }; 
+
+
